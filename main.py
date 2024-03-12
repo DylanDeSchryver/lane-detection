045de12e1f979c345d775847e2c71030a4cf1d67
DylanDeSchryver <142050322+DylanDeSchryver@users.noreply.github.com>
2024-03-12 08:40:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import cv2
 from lineDetect import contourDetection
-cap = cv2.VideoCapture('turnPWPVIDEO.mp4') # Initialize Camera
+cap = cv2.VideoCapture('white_road.mp4') # Initialize Camera
 
 while True:
     contourDetection(cap) # Line detection
 
-    if cv2.waitKey(1) & 0xFF == ord('q'): # if q is pressed, windows close and video stream ends. Program exits
+    if cv2.waitKey(50) & 0xFF == ord('q'): # if q is pressed, windows close and video stream ends. Program exits
         break
