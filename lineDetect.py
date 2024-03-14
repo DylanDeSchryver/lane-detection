3a8fc7bc85155ce22b9f3fcf821153a768343443
DylanDeSchryver <142050322+DylanDeSchryver@users.noreply.github.com>
2024-03-14 08:45:08 -0400

    Update lineDetect.py

--- lineDetect.py
+++ lineDetect.py
@@ -29,28 +29,55 @@ def contourDetection(cap):
     cv2.fillPoly(mask, polygon, 255)
     roi = cv2.bitwise_and(edged, mask)
 
-    # Apply the defined ROI on the processed edge image
+    # Perspective transform
+    pts1 = np.float32([[(int(width * 0.35), int(height * 0.90))], [(int(width * 0.48), int(height * 0.72))],
+                       [(int(width * 0.58), int(height * 0.72))], [(int(width * 0.8), int(height * 0.90))]])
+    pts2 = np.float32([[0,0], [500,600],
+                       [(int(width * 0.58), int(height * 0.72))], [(int(width * 0.8), int(height * 0.90))]])
+
+    # Apply Perspective Transform Algorithm
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)
+    result = cv2.warpPerspective(frame, matrix, (500, 600))
 
 
-    # Finding Contours
-    contours, hierarchy = cv2.findContours(roi, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    # Finding Lines
+    lines_list = []
+    lines = cv2.HoughLinesP(
+        roi,  # Input edge image
+        1,  # Distance resolution in pixels
+        np.pi / 180,  # Angle resolution in radians
+        threshold=20,  # Min number of votes for a valid line
+        minLineLength=1,  # Min allowed length of a line
+        maxLineGap=100  # Max allowed gap between line segments for joining them
+    )
+
+    # Iterate over detected lines
+    if lines is not None:
+        for line in lines:
+            # Extract line endpoints
+
+            x1, y1, x2, y2 = line[0]
+
+            #print(x1, y1, x2, y2)
+
+            lines_list.append((x1, y1, x2, y2))
+
+            cv2.line(frame,(x1,y1), (x2,y2), (0,255,0), 8)
 
     # Ensure that contours are found before proceeding
-    if len(contours) >= 2:
-        # Sort contours by area to find the largest ones (2 curved lines)
-        contours = sorted(contours, key=cv2.contourArea, reverse=True)[:4]
+    #if len(lines_list) >= 2:
 
-        # Draw all contours within the ROI
-        for contour in contours:
-            #contour += (r_x, r_y)  # Offset contour points to match ROI in the original frame
-            cv2.drawContours(frame, [contour], -1, (0, 255, 0), 4)
+        #for line in lines:
+
+            #cv2.line(frame, (new_x1, new_y1), (new_x2, new_y2), (0, 255, 0), 8)
 
     # draw midline
-    try:
-        cv2.polylines(frame, [np.array(midCalc(contours))], False, (0, 0, 255), 4)
-    except cv2.error:
-        pass
+    # try:
+    #     cv2.polylines(frame, [np.array(midCalc(contours))], False, (0, 0, 255), 4)
+    # except cv2.error:
+    #     pass
 
     # display frame
 
-    return cv2.imshow('Contours', frame), cv2.imshow('mask', mask), cv2.imshow('roi',roi)
+    return cv2.imshow('Contours', frame), cv2.imshow('warp', result)
